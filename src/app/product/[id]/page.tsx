2f9d41b776a77622c017cd264ea7009947cfef4e
nahead <gplaying780@gmail.com>
2025-01-29 21:05:57 -0800

    first commit

--- src/app/product/[id]/page.tsx
+++ src/app/product/[id]/page.tsx
@@ -44,108 +44,108 @@ export default async function ProductDetail({ params }: { params: { id: string }
 
   return (
     <div key={product._id}>
-    <BreadcrumbCollapsed />
-    <div className="flex flex-col md:flex-row justify-center  sm:justify-evenly sm:mt-10 p-5  sm:p-0 max-w-screen-2xl  mx-auto">
-      {/* left */}
-      <div className="flex sm:flex-col justify-between items-center w-full sm:w-[152px] order-2 sm:order-1">
-        {/* images */}
-        <Image
-          src={product.imageUrl}
-          className="w-[100px] sm:w-full h-[100px] sm:h-[150px]  rounded-[20px]"
-          alt={product.name}
-          width={550}
-          height={550}
-        ></Image>
-        <Image
-          src={product.imageUrl}
-          className="w-[100px] sm:w-full h-[100px] sm:h-[150px] sm:mt-3  rounded-[20px]"
-          alt={product.name}
-          width={550}
-          height={550}
-        ></Image>
-        <Image
-          src={product.imageUrl}
-          className="w-[100px] sm:w-full h-[100px] sm:h-[150px] sm:mt-3 rounded-[20px]"
-          alt={product.name}
-          width={550}
-          height={550}
-        ></Image>
-      </div>
-      {/* mid div */}
-      <div className="w-full sm:w-[444px] h-[260px] sm:h-[500px] mt-5 sm:mt-0 order-1 sm:order-2">
-        <Image
-          src={product.imageUrl}
-          alt={product.name}
-          className="w-full h-[95%] rounded-[20px]"
-          width={1150}
-          height={1150}
-        ></Image>
-      </div>
-      {/* right div */}
-      <div className="w-full sm:w-[500px] md:h-[500px] mt-3 order-3">
-        <h1 className="text-2xl md:text-3xl font-bold">{product.name}</h1>
-        {/* <div className="flex text-yellow-400">{star}</div> */}
-        <div className="flex text-yellow-400">
-                               {/* Map stars correctly */}
-                               {star.map((icon, index) => (
-                                 <span key={index}>{icon}</span>
-                               ))}
-                             </div>
-        <p className="font-bold mt-1">
-          {product.price}{" "}
-        </p>
-        <p>
-        {product.description}
-        </p>
-        {/* select color */}
-        <div className="mt-5">
-          <p className="text-gray-500">Select Colors</p>
-          <div className="flex space-x-3 mt-2">
-            <div className="w-[37px] h-[37px] bg-[#4F4631] rounded-full  flex justify-center items-center">
-              <Check className="text-white opacity-0  hover:opacity-100 cursor-pointer" />
-            </div>
-            <div className="w-[37px] h-[37px] bg-[#314F4A] rounded-full flex justify-center items-center">
-              <Check className="text-white opacity-0  hover:opacity-100 cursor-pointer" />
-            </div>
-            <div className="w-[37px] h-[37px] bg-[#31344F] rounded-full flex justify-center items-center">
-              <Check className="text-white opacity-0  hover:opacity-100 cursor-pointer" />
+      <BreadcrumbCollapsed />
+      <div className="flex flex-col md:flex-row justify-center  sm:justify-evenly sm:mt-10 p-5  sm:p-0 max-w-screen-2xl  mx-auto">
+        {/* left */}
+        <div className="flex sm:flex-col justify-between items-center w-full sm:w-[152px] order-2 sm:order-1">
+          {/* images */}
+          <Image
+            src={product.imageUrl}
+            className="w-[100px] sm:w-full h-[100px] sm:h-[150px]  rounded-[20px]"
+            alt={product.name}
+            width={550}
+            height={550}
+          ></Image>
+          <Image
+            src={product.imageUrl}
+            className="w-[100px] sm:w-full h-[100px] sm:h-[150px] sm:mt-3  rounded-[20px]"
+            alt={product.name}
+            width={550}
+            height={550}
+          ></Image>
+          <Image
+            src={product.imageUrl}
+            className="w-[100px] sm:w-full h-[100px] sm:h-[150px] sm:mt-3 rounded-[20px]"
+            alt={product.name}
+            width={550}
+            height={550}
+          ></Image>
+        </div>
+        {/* mid div */}
+        <div className="w-full sm:w-[444px] h-[260px] sm:h-[500px] mt-5 sm:mt-0 order-1 sm:order-2">
+          <Image
+            src={product.imageUrl}
+            alt={product.name}
+            className="w-full h-[95%] rounded-[20px]"
+            width={1150}
+            height={1150}
+          ></Image>
+        </div>
+        {/* right div */}
+        <div className="w-full sm:w-[500px] md:h-[500px] mt-3 order-3">
+          <h1 className="text-2xl md:text-3xl font-bold">{product.name}</h1>
+          {/* <div className="flex text-yellow-400">{star}</div> */}
+          <div className="flex text-yellow-400">
+            {/* Map stars correctly */}
+            {star.map((icon, index) => (
+              <span key={index}>{icon}</span>
+            ))}
+          </div>
+          <p className="font-bold mt-1">
+            {product.price}{" "}
+          </p>
+          <p>
+            {product.description}
+          </p>
+          {/* select color */}
+          <div className="mt-5">
+            <p className="text-gray-500">Select Colors</p>
+            <div className="flex space-x-3 mt-2">
+              <div className="w-[37px] h-[37px] bg-[#4F4631] rounded-full  flex justify-center items-center">
+                <Check className="text-white opacity-0  hover:opacity-100 cursor-pointer" />
+              </div>
+              <div className="w-[37px] h-[37px] bg-[#314F4A] rounded-full flex justify-center items-center">
+                <Check className="text-white opacity-0  hover:opacity-100 cursor-pointer" />
+              </div>
+              <div className="w-[37px] h-[37px] bg-[#31344F] rounded-full flex justify-center items-center">
+                <Check className="text-white opacity-0  hover:opacity-100 cursor-pointer" />
+              </div>
             </div>
           </div>
-        </div>
-        {/* Choose Size */}
-        <div className="mt-4">
-          <p className="text-gray-500">Choose Size</p>
-          <div className="flex space-x-3 mt-2">
-            <div className="w-[80px] h-[40px] flex justify-center items-center rounded-[62px] bg-[#F0F0F0] text-gray-400">
-              Small
-            </div>
-            <div className="w-[90px] h-[40px] flex justify-center items-center rounded-[62px] bg-[#F0F0F0] text-gray-400">
-              Medium
-            </div>
-            <div className="w-[80px] h-[40px] flex justify-center items-center rounded-[62px] bg-[#F0F0F0] text-gray-400">
-              Large
-            </div>
-            <div className="w-[90px] h-[40px] flex justify-center items-center rounded-[62px] bg-[#F0F0F0] text-gray-400">
-              X-Large
+          {/* Choose Size */}
+          <div className="mt-4">
+            <p className="text-gray-500">Choose Size</p>
+            <div className="flex space-x-3 mt-2">
+              <div className="w-[80px] h-[40px] flex justify-center items-center rounded-[62px] bg-[#F0F0F0] text-gray-400">
+                Small
+              </div>
+              <div className="w-[90px] h-[40px] flex justify-center items-center rounded-[62px] bg-[#F0F0F0] text-gray-400">
+                Medium
+              </div>
+              <div className="w-[80px] h-[40px] flex justify-center items-center rounded-[62px] bg-[#F0F0F0] text-gray-400">
+                Large
+              </div>
+              <div className="w-[90px] h-[40px] flex justify-center items-center rounded-[62px] bg-[#F0F0F0] text-gray-400">
+                X-Large
+              </div>
             </div>
           </div>
-        </div>
-        {/* BTNS */}
-        <div className="flex justify-start items-center mt-7 space-x-4">
-          <div className="w-[100px] h-[40px] flex justify-between p-3 items-center rounded-[62px] bg-[#F0F0F0] text-gray-400">
-            <Minus />
-            1
-            <Plus />
+          {/* BTNS */}
+          <div className="flex justify-start items-center mt-7 space-x-4">
+            <div className="w-[100px] h-[40px] flex justify-between p-3 items-center rounded-[62px] bg-[#F0F0F0] text-gray-400">
+              <Minus />
+              1
+              <Plus />
+            </div>
+            <Link href={`/cart/${product._id}`} key={product._id}>
+              <Detailpage product={product} />
+            </Link>
           </div>
-          <Link href={`/cart/${product._id}`} key={product._id}> 
-            <Detailpage product={product} />  
-          </Link>
         </div>
       </div>
+      <CustomerTestimonials />
+      <T_shirts />
     </div>
-    <CustomerTestimonials />
-    <T_shirts />
-  </div>
   );
 }
 
